c4edf4a13a2d6934194168eaa9a3a7516c0dd92a
Jordan Lys <lys.jordan@gmail.com>
2022-12-21 12:41:14 -0600

    add support for use and message

--- tests/index.js
+++ tests/index.js
@@ -5,7 +5,7 @@ function testValidateSchema() {
 
   const requiredSchema = {
     name: {
-      first: { type: String, required: true },
+      first: { type: String, required: true, use:  val => ['Tom'].includes(val), message: 'name.first must be Tom'},
       last: { type: String, required: true }
     },
     age: { type : Number }
@@ -22,19 +22,31 @@ function testValidateSchema() {
   }
 
   function shouldFailForWrongObject() {
-    let person = { name: { first: 10, last: 'Xoman' }, age: 'Tom' }
+    let person = { name: { first: 2, last: 'Xoman' }, age: 'Tom' }
     schemaErrors = validateSchema(person, { schema: requiredSchema, logLevel: 'none' })
     if(!Array.isArray(schemaErrors)) {
       assert.fail('Returned object must be an array')
     }
-    assert.strictEqual(schemaErrors.length, 2)
+    assert.strictEqual(schemaErrors.length, 3)
     assert.ok(schemaErrors.some(err => err.message === 'age must be of type Number.'))
-    assert.ok(schemaErrors.some(err => err.message === 'name.first must be of type String.'))
+    assert.ok(schemaErrors.some(err => err.message === 'name.first must be Tom'))
+  }
+
+
+  function shouldFailForWrongStringLiteral() {
+    let person = { name: { first: 'NotTom', last: 'Xoman' }, age: 45 }
+    schemaErrors = validateSchema(person, { schema: requiredSchema, logLevel: 'none' })
+    if(!Array.isArray(schemaErrors)) {
+      assert.fail('Returned object must be an array')
+    }
+    assert.strictEqual(schemaErrors.length, 1)
+    assert.ok(schemaErrors.some(err => err.message === 'name.first must be Tom'))
   }
 
   // running all tests
-  shouldPassForCorrectObject()
-  shouldFailForWrongObject()
+  shouldPassForCorrectObject();
+  shouldFailForWrongObject();
+  shouldFailForWrongStringLiteral();
 }
 
 testValidateSchema()
